eb1bea2fa83d4143ca0cc0385d3fb53f43fc6e7d
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:11 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api.Core.Models/Undocumented/TwitchPrimeOffers/TwitchPrimeOffers.cs
@@ -0,0 +1,10 @@
+﻿namespace TwitchLib.Api.Core.Models.Undocumented.TwitchPrimeOffers
+{
+    using Newtonsoft.Json;
+
+    public class TwitchPrimeOffers
+    {
+        [JsonProperty(PropertyName = "offers")]
+        public Offer[] Offers { get; protected set; }
+    }
+}
