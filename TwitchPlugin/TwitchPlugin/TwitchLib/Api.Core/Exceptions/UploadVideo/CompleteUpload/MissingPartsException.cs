3a6fe80260a19476c3706db5187ee5ea76992ca9
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:14 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api.Core/Exceptions/UploadVideo/CompleteUpload/MissingPartsException.cs
@@ -0,0 +1,16 @@
+﻿namespace TwitchLib.Api.Core.Exceptions.UploadVideo.CompleteUpload
+{
+    using System;
+
+    /// <inheritdoc />
+    /// <summary>Exception thrown attempting to finish an upload without all parts.</summary>
+    public class MissingPartsException : Exception
+    {
+        /// <inheritdoc />
+        /// <summary>Exception constructor</summary>
+        public MissingPartsException(String apiData)
+            : base(apiData)
+        {
+        }
+    }
+}
