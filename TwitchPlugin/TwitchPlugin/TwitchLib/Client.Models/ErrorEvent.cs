e5a20a21a1eec3fd76ce54476e5fc2208684be67
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:38 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Client.Models/ErrorEvent.cs
@@ -0,0 +1,11 @@
+﻿namespace TwitchLib.Client.Models
+{
+    using System;
+
+    /// <summary>Class representing the error that the websocket encountered.</summary>
+    public class ErrorEvent
+    {
+        /// <summary>Message pertaining to the error.</summary>
+        public String Message { get;  set; }        
+    }
+}
