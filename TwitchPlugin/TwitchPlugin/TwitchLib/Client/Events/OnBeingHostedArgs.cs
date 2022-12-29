a28a0193ed44f256b928af231bc20023508f7357
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:43 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Client/Events/OnBeingHostedArgs.cs
@@ -0,0 +1,13 @@
+﻿namespace TwitchLib.Client.Events
+{
+    using System;
+    using Models;
+
+    /// <inheritdoc />
+    /// <summary>Args representing an event where another channel has started hosting the broadcaster's channel.</summary>
+    public class OnBeingHostedArgs : EventArgs
+    {
+        /// <summary>Property representing the Host notification</summary>
+        public BeingHostedNotification BeingHostedNotification;
+    }
+}
