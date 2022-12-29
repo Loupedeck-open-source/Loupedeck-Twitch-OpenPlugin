42292b27d9a4e18e06f21efe6d8e964b2245e8e2
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:45 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Client/Events/OnHostingStoppedArgs.cs
@@ -0,0 +1,13 @@
+﻿namespace TwitchLib.Client.Events
+{
+    using System;
+    using Models;
+
+    /// <inheritdoc />
+    /// <summary>Args representing hosting stopped event.</summary>
+    public class OnHostingStoppedArgs : EventArgs
+    {
+        /// <summary>Property representing hosting channel.</summary>
+        public HostingStopped HostingStopped;
+    }
+}
