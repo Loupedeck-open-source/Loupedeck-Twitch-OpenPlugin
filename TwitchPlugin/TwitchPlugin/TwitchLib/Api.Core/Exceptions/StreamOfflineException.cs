e67723cca6ea5637dff9d49075093b68f98b5477
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:13 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api.Core/Exceptions/StreamOfflineException.cs
@@ -0,0 +1,13 @@
+﻿namespace TwitchLib.Api.Core.Exceptions
+{
+    using System;
+
+    /// <inheritdoc />
+    /// <summary>Exception representing an attempt to fetch stream data on a stream that is offline.</summary>
+    public class StreamOfflineException : Exception
+    {
+        /// <inheritdoc />
+        /// <summary>Exception constructor</summary>
+        public StreamOfflineException(String apiData) : base(apiData) { }
+    }
+}
