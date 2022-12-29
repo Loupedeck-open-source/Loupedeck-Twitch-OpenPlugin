f38da1ee3b999ef5adc9a809fbf123d5d3a02e04
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:49 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Client/Exceptions/BadListenException.cs
@@ -0,0 +1,18 @@
+﻿namespace TwitchLib.Client.Exceptions
+{
+    using System;
+
+    /// <inheritdoc />
+    /// <summary>Exception thrown when an event is subscribed to when it shouldn't be.</summary>
+    public class BadListenException : Exception
+    {
+        /// <inheritdoc />
+        /// <summary>Exception constructor</summary>
+        public BadListenException(String eventName, String additionalDetails = "")
+            : base(String.IsNullOrEmpty(additionalDetails)
+                ? $"You are listening to event '{eventName}', which is not currently allowed. See details: {additionalDetails}"
+                : $"You are listening to event '{eventName}', which is not currently allowed.")
+        {
+        }
+    }
+}
