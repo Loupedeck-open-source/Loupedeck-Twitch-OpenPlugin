8dd1b011ca3594e0f0d293b15a03c992fbceba44
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:20 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api.Helix.Models/Clips/GetClip/GetClipResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace TwitchLib.Api.Helix.Models.Clips.GetClip
+{
+    using Newtonsoft.Json;
+
+    public class GetClipResponse
+    {
+        [JsonProperty(PropertyName = "data")]
+        public Clip[] Clips { get; protected set; }
+    }
+}
