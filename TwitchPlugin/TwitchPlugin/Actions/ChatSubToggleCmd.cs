bc21e89dcb78cc6533f3eab65f9e4ab8f30e4b97
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:50:44 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/Actions/ChatSubToggleCmd.cs
@@ -0,0 +1,34 @@
+﻿namespace Loupedeck.TwitchPlugin.Actions
+{
+    using System;
+
+    internal class ChatSubToggleCmd : GenericOnOffSwitch
+    {
+        public ChatSubToggleCmd()
+                        : base(
+                            name: "ToggleSubscribersOnly1",
+                            displayName: "(new) Chat Subscribers-Only",
+                            description: "Toggles chat Subscribers-only mode",
+                            groupName: "",
+                            offStateName: "Chat Subscribers-Only On",
+                            onStateName: "Chat Subscribers-Only Off",
+                            offStateImage: "TwitchSubChat2.png",
+                            onStateImage: "TwitchSubChat.png")
+        {
+        }
+
+        protected override void ConnectAppEvents(EventHandler<EventArgs> eventSwitchedOff, EventHandler<EventArgs> eventSwitchedOn)
+        {
+            TwitchPlugin.Proxy.AppEvtChatSubscribersOnlyOff += eventSwitchedOn;
+            TwitchPlugin.Proxy.AppEvtChatSubscribersOnlyOn += eventSwitchedOff;
+        }
+
+        protected override void DisconnectAppEvents(EventHandler<EventArgs> eventSwitchedOff, EventHandler<EventArgs> eventSwitchedOn)
+        {
+            TwitchPlugin.Proxy.AppEvtChatSubscribersOnlyOff -= eventSwitchedOn;
+            TwitchPlugin.Proxy.AppEvtChatSubscribersOnlyOn -= eventSwitchedOff;
+        }
+
+        protected override void RunToggle() => TwitchPlugin.Proxy.AppToggleSubscribersOnly();
+    }
+}
