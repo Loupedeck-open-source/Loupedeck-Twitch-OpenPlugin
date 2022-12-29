009e6e5ba3d98e67a591ea6814d036f252650abf
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:01 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api.Core.Interfaces/ITime.cs
@@ -0,0 +1,13 @@
+﻿namespace TwitchLib.Api.Core.Interfaces
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public interface ITime
+    {
+        DateTime GetTimeNow();
+
+        Task GetDelay(TimeSpan timespan, CancellationToken cancellationToken);
+    }
+}
