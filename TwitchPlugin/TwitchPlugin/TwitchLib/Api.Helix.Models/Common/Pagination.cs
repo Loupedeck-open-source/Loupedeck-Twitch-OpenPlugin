3e8ace53518b8fba0132d8d282cd1babd12f62af
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:20 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api.Helix.Models/Common/Pagination.cs
@@ -0,0 +1,11 @@
+﻿namespace TwitchLib.Api.Helix.Models.Common
+{
+    using System;
+    using Newtonsoft.Json;
+
+    public class Pagination
+    {
+        [JsonProperty(PropertyName = "cursor")]
+        public String Cursor { get; protected set; }
+    }
+}
