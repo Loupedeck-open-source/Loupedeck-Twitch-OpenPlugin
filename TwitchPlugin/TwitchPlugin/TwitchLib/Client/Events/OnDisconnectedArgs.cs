8b05eb3f4217d31a4cffb9d2a545d2f86b9ac8db
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:44 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Client/Events/OnDisconnectedArgs.cs
@@ -0,0 +1,12 @@
+﻿namespace TwitchLib.Client.Events
+{
+    using System;
+
+    /// <inheritdoc />
+    /// <summary>Args representing client disconnect event.</summary>
+    public class OnDisconnectedArgs : EventArgs
+    {
+        /// <summary>Username of the bot that was disconnected.</summary>
+        public String BotUsername;
+    }
+}
