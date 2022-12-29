2dffa2f72f7dadb4ac51eebbcf0fab14177d276f
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:14 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api.Core/Exceptions/UploadVideo/CreateVideo/UnauthorizedException.cs
@@ -0,0 +1,16 @@
+﻿namespace TwitchLib.Api.Core.Exceptions.UploadVideo.CreateVideo
+{
+    using System;
+
+    /// <inheritdoc />
+    /// <summary>Exception thrown when the passed access token doesn't have the correct scope.</summary>
+    public class UnauthorizedException : Exception
+    {
+        /// <inheritdoc />
+        /// <summary>Exception constructor</summary>
+        public UnauthorizedException(String apiData)
+            : base(apiData)
+        {
+        }
+    }
+}
