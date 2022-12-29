8627af62a416b0ed7618d92215364dfdb1efbf0a
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:45 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Client/Events/OnHostingStartedArgs.cs
@@ -0,0 +1,13 @@
+﻿namespace TwitchLib.Client.Events
+{
+    using System;
+    using Models;
+
+    /// <inheritdoc />
+    /// <summary>Args representing hosting started event.</summary>
+    public class OnHostingStartedArgs : EventArgs
+    {
+        /// <summary>Property representing hosting channel.</summary>
+        public HostingStarted HostingStarted;
+    }
+}
