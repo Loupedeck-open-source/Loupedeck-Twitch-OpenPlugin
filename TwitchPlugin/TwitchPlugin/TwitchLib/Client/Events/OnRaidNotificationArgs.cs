869fa11bce9a952c76d44b2649c03da0b52ced6c
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:47 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Client/Events/OnRaidNotificationArgs.cs
@@ -0,0 +1,11 @@
+﻿namespace TwitchLib.Client.Events
+{
+    using System;
+    using Models;
+
+    public class OnRaidNotificationArgs : EventArgs
+    {
+        public RaidNotification RaidNotificaiton;
+        public String Channel;
+    }
+}
