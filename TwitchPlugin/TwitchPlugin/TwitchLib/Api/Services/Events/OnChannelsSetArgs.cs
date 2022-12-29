075825ce76633a8fe0e2a7c39dc4e49b185785dc
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:31 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api/Services/Events/OnChannelsSetArgs.cs
@@ -0,0 +1,17 @@
+﻿namespace TwitchLib.Api.Services.Events
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <inheritdoc />
+    /// <summary>
+    /// Class representing EventArgs for the OnChannelsSet event.
+    /// </summary>
+    public class OnChannelsSetArgs : EventArgs
+    {
+        /// <summary>
+        /// The channels the service is currently monitoring.
+        /// </summary>
+        public List<String> Channels;
+    }
+}
