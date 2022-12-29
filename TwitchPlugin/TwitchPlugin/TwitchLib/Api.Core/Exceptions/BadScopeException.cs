246afbd5ddc0b5551880ab3114567b2c9848de59
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:12 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api.Core/Exceptions/BadScopeException.cs
@@ -0,0 +1,16 @@
+﻿namespace TwitchLib.Api.Core.Exceptions
+{
+    using System;
+
+    /// <inheritdoc />
+    /// <summary>Exception representing a provided scope was not permitted.</summary>
+    public class BadScopeException : Exception
+    {
+        /// <inheritdoc />
+        /// <summary>Exception constructor</summary>
+        public BadScopeException(String data)
+            : base(data)
+        {
+        }
+    }
+}
