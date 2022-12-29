cb679c44e1ee2d6bff1a47958392b1dacae5f271
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:50 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Client/Exceptions/ErrorLoggingInException.cs
@@ -0,0 +1,20 @@
+﻿namespace TwitchLib.Client.Exceptions
+{
+    using System;
+
+    /// <inheritdoc />
+    /// <summary>Exception representing credentials provided for logging in were bad.</summary>
+    public class ErrorLoggingInException : Exception
+    {
+        /// <summary>Exception representing username associated with bad login.</summary>
+        public String Username { get; protected set; }
+
+        /// <inheritdoc />
+        /// <summary>Exception construtor.</summary>
+        public ErrorLoggingInException(String ircData, String twitchUsername)
+            : base(ircData)
+        {
+            this.Username = twitchUsername;
+        }
+    }
+}
