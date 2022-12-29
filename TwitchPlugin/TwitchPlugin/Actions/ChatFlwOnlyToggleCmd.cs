d01ade1e3564554930b1ea380afb649cba5d937c
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:50:44 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/Actions/ChatFlwOnlyToggleCmd.cs
@@ -0,0 +1,35 @@
+﻿namespace Loupedeck.TwitchPlugin.Actions
+{
+    using System;
+
+    internal class ChatFlwToggleCmd : GenericOnOffSwitch
+    {
+        //FIXME: Add Parameters for Minutes
+        public ChatFlwToggleCmd()
+                        : base(
+                            name: "ToggleFollowersOnlyList1",
+                            displayName: "Chat Followers-Only",
+                            description: "Toggles chat emotes-only mode",
+                            groupName: "",
+                            offStateName: "Chat Followers-Only On",
+                            onStateName: "Chat Followers-Only Off",
+                            offStateImage: "TwitchFollowerChat.png",
+                            onStateImage: "TwitchFollowerChatToggle.png")
+        {
+        }
+
+        protected override void ConnectAppEvents(EventHandler<EventArgs> eventSwitchedOff, EventHandler<EventArgs> eventSwitchedOn)
+        {
+            TwitchPlugin.Proxy.AppEvtChatFollowersOnlyOff += eventSwitchedOn;
+            TwitchPlugin.Proxy.AppEvtChatFollowersOnlyOn += eventSwitchedOff;
+        }
+
+        protected override void DisconnectAppEvents(EventHandler<EventArgs> eventSwitchedOff, EventHandler<EventArgs> eventSwitchedOn)
+        {
+            TwitchPlugin.Proxy.AppEvtChatFollowersOnlyOff -= eventSwitchedOn;
+            TwitchPlugin.Proxy.AppEvtChatFollowersOnlyOn -= eventSwitchedOff;
+        }
+
+        protected override void RunToggle() => TwitchPlugin.Proxy.AppToggleFollowersOnly();
+    }
+}
