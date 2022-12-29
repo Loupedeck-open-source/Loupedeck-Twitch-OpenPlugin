f59a48d704d39fe6b696a2d3c9dcf38a6f0b490f
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:48 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Client/Events/OnUserTimedoutArgs.cs
@@ -0,0 +1,12 @@
+﻿namespace TwitchLib.Client.Events
+{
+    using System;
+    using Models;
+
+    /// <inheritdoc />
+    /// <summary>Args representing a user was timed out event.</summary>
+    public class OnUserTimedoutArgs : EventArgs
+    {
+        public UserTimeout UserTimeout;
+    }
+}
