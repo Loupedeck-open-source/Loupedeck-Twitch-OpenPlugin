db86dbec9384c09cf2fc70e91226878f67b2fa44
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:22 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api.Helix.Models/Streams/LiveStreams.cs
@@ -0,0 +1,17 @@
+﻿namespace TwitchLib.Api.Helix.Models.Streams
+{
+    using System;
+    using Newtonsoft.Json;
+
+    public class LiveStreams
+    {
+        #region Total
+        [JsonProperty(PropertyName = "_total")]
+        public Int32 Total { get; protected set; }
+        #endregion
+        #region Streams
+        [JsonProperty(PropertyName = "streams")]
+        public Stream[] Streams { get; protected set; }
+        #endregion
+    }
+}
