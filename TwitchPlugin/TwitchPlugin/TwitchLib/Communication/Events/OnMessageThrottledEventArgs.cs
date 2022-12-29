c8780b1daccb83349a008bf8909ecc5facdbde7a
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:56 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Communication/Events/OnMessageThrottledEventArgs.cs
@@ -0,0 +1,12 @@
+﻿namespace TwitchLib.Communication.Events
+{
+    using System;
+
+    public class OnMessageThrottledEventArgs : EventArgs
+    {
+        public String Message { get; set; }
+        public Int32 SentMessageCount { get; set; }
+        public TimeSpan Period { get; set; }
+        public Int32 AllowedInPeriod { get; set; }
+    }
+}
