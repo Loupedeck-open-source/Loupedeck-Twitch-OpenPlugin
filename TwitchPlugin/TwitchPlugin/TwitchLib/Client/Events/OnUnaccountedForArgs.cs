12d7405f3a2e3960702f7c1627f845369d238a2c
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:48 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Client/Events/OnUnaccountedForArgs.cs
@@ -0,0 +1,12 @@
+﻿namespace TwitchLib.Client.Events
+{
+    using System;
+
+    public class OnUnaccountedForArgs : EventArgs
+    {
+        public String RawIRC { get; set; }
+        public String Location { get; set; }
+        public String BotUsername { get; set; } // may not be available
+        public String Channel { get; set; } // may not be available
+    }
+}
