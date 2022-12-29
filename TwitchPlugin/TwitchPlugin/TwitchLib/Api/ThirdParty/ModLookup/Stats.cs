7c37eac22a4daf2a379d1a69de0fe0c0c1bf83a1
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:33 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api/ThirdParty/ModLookup/Stats.cs
@@ -0,0 +1,19 @@
+﻿namespace TwitchLib.Api.ThirdParty.ModLookup
+{
+    using System;
+    using Newtonsoft.Json;
+
+    public class Stats
+    {
+        [JsonProperty(PropertyName = "relations")]
+        public Int32 Relations { get; protected set; }
+        [JsonProperty(PropertyName = "channels_total")]
+        public Int32 ChannelsTotal { get; protected set; }
+        [JsonProperty(PropertyName = "users")]
+        public Int32 Users { get; protected set; }
+        [JsonProperty(PropertyName = "channels_no_mods")]
+        public Int32 ChannelsNoMods { get; protected set; }
+        [JsonProperty(PropertyName = "channels_only_broadcaster")]
+        public Int32 ChannelsOnlyBroadcaster { get; protected set; }
+    }
+}
