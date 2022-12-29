2560c37352017228b4c3a5b67dae61008af4ec8e
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:25 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api.Helix.Models/Users/Internal/UserActiveExtension.cs
@@ -0,0 +1,21 @@
+﻿namespace TwitchLib.Api.Helix.Models.Users.Internal
+{
+    using System;
+    using Newtonsoft.Json;
+
+    public class UserActiveExtension
+    {
+        [JsonProperty(PropertyName = "active")]
+        public Boolean Active { get; protected set; }
+        [JsonProperty(PropertyName = "id")]
+        public String Id { get; protected set; }
+        [JsonProperty(PropertyName = "version")]
+        public String Version { get; protected set; }
+        [JsonProperty(PropertyName = "name")]
+        public String Name { get; protected set; }
+        [JsonProperty(PropertyName = "x")]
+        public Int32 X { get; protected set; }
+        [JsonProperty(PropertyName = "y")]
+        public Int32 Y { get; protected set; }
+    }
+}
