fa0a2f4bc02965a5ef046d78b3192d1ce37840e7
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:36 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Client.Enums/StringEnum.cs
@@ -0,0 +1,21 @@
+﻿namespace TwitchLib.Client.Enums
+{
+    using System;
+
+    ///<summary>Really cool way of doing string enums, credits to RogueException (https://github.com/RogueException)</summary>
+    public abstract class StringEnum
+    {
+        /// <summary>Value of enum</summary>
+        public String Value { get; }
+
+        /// <summary>StringEnum constructor.</summary>
+        protected StringEnum(String value)
+        {
+            this.Value = value;
+        }
+
+        /// <summary>Returns string value for overriden ToString()</summary>
+        /// <returns>Enum value</returns>
+        public override String ToString() => this.Value;
+    }
+}
