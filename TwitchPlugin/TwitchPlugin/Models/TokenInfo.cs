aa61c8654087472c5e9d635bbfdfc02abadb4a6e
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:50:54 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/Models/TokenInfo.cs
@@ -0,0 +1,21 @@
+﻿namespace Loupedeck.TwitchPlugin
+{
+    using System;
+    using System.Collections.Generic;
+    using Newtonsoft.Json;
+
+    public class TokenInfo
+    {
+        [JsonProperty("client_id")]
+        public String ClientId { get; set; }
+
+        [JsonProperty("login")]
+        public String Login { get; set; }
+
+        [JsonProperty("scopes")]
+        public List<String> Scopes { get; set; }
+
+        [JsonProperty("user_id")]
+        public Int32 UserId { get; set; }
+    }
+}
