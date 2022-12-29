f7b24c8decbcb6120b04549b3e9e52ab555af2be
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:30 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api/Services/Events/FollowerService/OnNewFollowersDetectedArgs.cs
@@ -0,0 +1,18 @@
+﻿namespace TwitchLib.Api.Services.Events.FollowerService
+{
+    using System;
+    using System.Collections.Generic;
+    using Helix.Models.Users;
+
+    /// <inheritdoc />
+    /// <summary>
+    /// Class representing EventArgs for OnNewFollowersDetected event.
+    /// </summary>
+    public class OnNewFollowersDetectedArgs : EventArgs
+    {
+        /// <summary>Event property representing channel the service is currently monitoring.</summary>
+        public String Channel;
+        /// <summary>Event property representing all new followers detected.</summary>
+        public List<Follow> NewFollowers;
+    }
+}
