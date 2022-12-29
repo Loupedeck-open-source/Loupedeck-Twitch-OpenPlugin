4bd1400ccc3f0b06d9a1ccb0a6e7c560ec5d5fb4
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:06 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api.Core.Models/Undocumented/ChannelExtensionData/Views.cs
@@ -0,0 +1,12 @@
+﻿namespace TwitchLib.Api.Core.Models.Undocumented.ChannelExtensionData
+{
+    using Newtonsoft.Json;
+
+    public class Views
+    {
+        [JsonProperty(PropertyName = "video_overlay")]
+        public View VideoOverlay { get; protected set; }
+        [JsonProperty(PropertyName = "config")]
+        public View Config { get; protected set; }
+    }
+}
