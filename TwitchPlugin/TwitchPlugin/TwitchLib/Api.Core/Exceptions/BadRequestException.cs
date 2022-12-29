ed5ffe37f9deba7d89b3a2d7c46db3c34fc86a66
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:12 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api.Core/Exceptions/BadRequestException.cs
@@ -0,0 +1,16 @@
+﻿namespace TwitchLib.Api.Core.Exceptions
+{
+    using System;
+
+    /// <inheritdoc />
+    /// <summary>Exception representing a request that doesn't have a clientid attached.</summary>
+    public class BadRequestException : Exception
+    {
+        /// <inheritdoc />
+        /// <summary>Exception constructor</summary>
+        public BadRequestException(String apiData)
+            : base(apiData)
+        {
+        }
+    }
+}
