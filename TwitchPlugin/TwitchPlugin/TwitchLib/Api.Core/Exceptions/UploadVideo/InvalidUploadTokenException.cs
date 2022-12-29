cd21c05553b4d29d3a7fda2754f98cd99eb96f73
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:15 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api.Core/Exceptions/UploadVideo/InvalidUploadTokenException.cs
@@ -0,0 +1,16 @@
+﻿namespace TwitchLib.Api.Core.Exceptions.UploadVideo
+{
+    using System;
+
+    /// <inheritdoc />
+    /// <summary>Exception thrown when the identifying video token is invalid.</summary>
+    public class InvalidUploadTokenException : Exception
+    {
+        /// <inheritdoc />
+        /// <summary>Exception constructor</summary>
+        public InvalidUploadTokenException(String apiData)
+            : base(apiData)
+        {
+        }
+    }
+}
