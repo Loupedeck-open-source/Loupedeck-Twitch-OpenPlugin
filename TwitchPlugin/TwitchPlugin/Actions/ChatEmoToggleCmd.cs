d69af042b47b6ac7c82533c0e0d4aa32b1230853
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:50:44 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/Actions/ChatEmoToggleCmd.cs
@@ -0,0 +1,34 @@
+﻿namespace Loupedeck.TwitchPlugin.Actions
+{
+    using System;
+
+    internal class ChatEmoToggleCmd : GenericOnOffSwitch
+    {
+        public ChatEmoToggleCmd()
+                        : base(
+                            name: "ToggleEmotesOnly1",
+                            displayName: "(new) Chat Emotes-Only",
+                            description: "Toggles chat emotes-only mode",
+                            groupName: "",
+                            offStateName: "Chat Emotes-Only On",
+                            onStateName: "Chat Emotes-Only Off",
+                            offStateImage: "TwitchEmoteChat.png",
+                            onStateImage: "TwitchEmoteChatToggle.png")
+        {
+        }
+
+        protected override void ConnectAppEvents(EventHandler<EventArgs> eventSwitchedOff, EventHandler<EventArgs> eventSwitchedOn)
+        {
+            TwitchPlugin.Proxy.AppEvtChatEmotesOnlyOff += eventSwitchedOn;
+            TwitchPlugin.Proxy.AppEvtChatEmotesOnlyOn += eventSwitchedOff;
+        }
+
+        protected override void DisconnectAppEvents(EventHandler<EventArgs> eventSwitchedOff, EventHandler<EventArgs> eventSwitchedOn)
+        {
+            TwitchPlugin.Proxy.AppEvtChatEmotesOnlyOff -= eventSwitchedOn;
+            TwitchPlugin.Proxy.AppEvtChatEmotesOnlyOn -= eventSwitchedOff;
+        }
+
+        protected override void RunToggle() => TwitchPlugin.Proxy.AppToggleEmotesOnly();
+    }
+}
