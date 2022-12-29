1d0b4214d706befbf23fc7b25880b6a4634fa42b
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:02 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api.Core.Models/Root/Root.cs
@@ -0,0 +1,13 @@
+﻿namespace TwitchLib.Api.Core.Models.Root
+{
+    using Newtonsoft.Json;
+
+    public class Root
+    {
+        #region Token
+        /// <summary>Property representing token object.</summary>
+        [JsonProperty(PropertyName = "token")]
+        public RootToken Token { get; protected set; }
+        #endregion
+    }
+}
