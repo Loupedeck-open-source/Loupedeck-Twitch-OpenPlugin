7b3705017156a0996515b6b412d3914db1e02672
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:50 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Client/Exceptions/EventNotHandledException.cs
@@ -0,0 +1,18 @@
+﻿namespace TwitchLib.Client.Exceptions
+{
+    using System;
+
+    /// <inheritdoc />
+    /// <summary>Exception thrown when an event that is not handled is required to be handled.</summary>
+    public class EventNotHandled : Exception
+    {
+        /// <inheritdoc />
+        /// <summary>Exception constructor</summary>
+        public EventNotHandled(String eventName, String additionalDetails = "")
+            : base(String.IsNullOrEmpty(additionalDetails) 
+                  ? $"To use this call, you must handle/subscribe to event: {eventName}" 
+                  : $"To use this call, you must handle/subscribe to event: {eventName}, additional details: {additionalDetails}")
+        {
+        }
+    }
+}
