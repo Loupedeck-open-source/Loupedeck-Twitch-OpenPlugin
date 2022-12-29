9f2e9bdd7c5fc01ea74142f447acd01eb0c2437a
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:17 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api.Core/RateLimiter/DisposeAction.cs
@@ -0,0 +1,20 @@
+﻿namespace TwitchLib.Api.Core.RateLimiter
+{
+    using System;
+
+    public class DisposeAction : IDisposable
+    {
+        private Action _act;
+
+        public DisposeAction(Action act) 
+        {
+            this._act = act;
+        }
+
+        public void Dispose() 
+        {
+            this._act?.Invoke();
+            this._act = null;
+        }
+    }
+}
