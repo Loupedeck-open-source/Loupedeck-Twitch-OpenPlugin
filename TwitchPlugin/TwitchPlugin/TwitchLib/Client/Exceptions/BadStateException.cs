a9cb0a350104d565dfc4a2bb92f734d9284a8303
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:49 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Client/Exceptions/BadStateException.cs
@@ -0,0 +1,16 @@
+﻿namespace TwitchLib.Client.Exceptions
+{
+    using System;
+
+    /// <inheritdoc />
+    /// <summary>Exception thrown when the state of the client cannot allow an operation to be run.</summary>
+    public class BadStateException : Exception
+    {
+        /// <inheritdoc />
+        /// <summary>Exception constructor</summary>
+        public BadStateException(String details)
+            : base(details)
+        {
+        }
+    }
+}
