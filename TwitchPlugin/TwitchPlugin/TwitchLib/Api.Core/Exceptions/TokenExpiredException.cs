fb1a20460d1eadda7a29fdc0205e071f0da84cb7
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:14 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api.Core/Exceptions/TokenExpiredException.cs
@@ -0,0 +1,16 @@
+﻿namespace TwitchLib.Api.Core.Exceptions
+{
+    using System;
+
+    /// <inheritdoc />
+    /// <summary>Exception representing a detection that the OAuth token expired</summary>
+    public class TokenExpiredException : Exception
+    {
+        /// <inheritdoc />
+        /// <summary>Exception constructor</summary>
+        public TokenExpiredException(String data)
+            : base(data)
+        {
+        }
+    }
+}
