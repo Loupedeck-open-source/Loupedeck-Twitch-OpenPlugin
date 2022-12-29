88f9d6bc79cb4a15660d66ec0445587303eea01d
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:13 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api.Core/Exceptions/GatewayTimeoutException.cs
@@ -0,0 +1,16 @@
+﻿namespace TwitchLib.Api.Core.Exceptions
+{
+    using System;
+
+    /// <inheritdoc />
+    /// <summary>Exception representing a 504 Http Statuscode</summary>
+    public class GatewayTimeoutException : Exception
+    {
+        /// <inheritdoc />
+        /// <summary>Exception constructor</summary>
+        public GatewayTimeoutException(String data)
+            : base(data)
+        {
+        }
+    }
+}
