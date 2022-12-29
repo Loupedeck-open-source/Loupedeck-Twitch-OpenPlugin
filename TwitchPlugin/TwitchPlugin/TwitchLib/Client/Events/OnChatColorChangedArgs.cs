cc6d5074d9b4576d4ed13a3e41da94d83565b003
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:43 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Client/Events/OnChatColorChangedArgs.cs
@@ -0,0 +1,12 @@
+﻿namespace TwitchLib.Client.Events
+{
+    using System;
+
+    /// <inheritdoc />
+    /// <summary>Args representing a successful chat color change request.</summary>
+    public class OnChatColorChangedArgs : EventArgs
+    {
+        /// <summary>Property reprenting the channel the event was received in.</summary>
+        public String Channel;
+    }
+}
