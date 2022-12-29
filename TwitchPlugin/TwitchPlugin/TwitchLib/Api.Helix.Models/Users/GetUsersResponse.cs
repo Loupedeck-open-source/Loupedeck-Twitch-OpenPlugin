56f9f94e96a5af6959f299f5cf3ff1c99a841e77
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:24 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api.Helix.Models/Users/GetUsersResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace TwitchLib.Api.Helix.Models.Users
+{
+    using Newtonsoft.Json;
+
+    public class GetUsersResponse
+    {
+        [JsonProperty(PropertyName = "data")]
+        public User[] Users { get; protected set; }
+    }
+}
