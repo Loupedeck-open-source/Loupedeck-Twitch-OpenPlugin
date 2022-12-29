85d0947e4fbb82fb96065ec6f108cb79ea6bb051
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:19 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api.Helix.Models/Analytics/GetExtensionAnalyticsResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace TwitchLib.Api.Helix.Models.Analytics
+{
+    using Newtonsoft.Json;
+
+    public class GetExtensionAnalyticsResponse
+    {
+        [JsonProperty(PropertyName = "data")]
+        public ExtensionAnalytics[] Data { get; protected set; }
+    }
+}
