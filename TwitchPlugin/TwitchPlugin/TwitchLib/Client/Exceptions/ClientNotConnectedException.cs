aa6de0239322502463d032b59d2edc04448086a4
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:50 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Client/Exceptions/ClientNotConnectedException.cs
@@ -0,0 +1,16 @@
+﻿namespace TwitchLib.Client.Exceptions
+{
+    using System;
+
+    /// <inheritdoc />
+    /// <summary>Exception thrown when attempting to perform an actino that is only available when the client is connected.</summary>
+    public class ClientNotConnectedException : Exception
+    {
+        /// <inheritdoc />
+        /// <summary>Exception constructor</summary>
+        public ClientNotConnectedException(String description)
+            : base(description)
+        {
+        }
+    }
+}
