e983ec14aaae6e2bb490eac3f26487e9fa95c1d2
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:47 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Client/Events/OnReSubscriberArgs.cs
@@ -0,0 +1,15 @@
+﻿namespace TwitchLib.Client.Events
+{
+    using System;
+    using Models;
+
+    /// <inheritdoc />
+    /// <summary>Args representing resubscriber event.</summary>
+    public class OnReSubscriberArgs : EventArgs
+    {
+        /// <summary>Property representing resubscriber object.</summary>
+        public ReSubscriber ReSubscriber;
+        /// <summary>Property representing the Twitch channel this event fired from.</summary>
+        public String Channel;
+    }
+}
