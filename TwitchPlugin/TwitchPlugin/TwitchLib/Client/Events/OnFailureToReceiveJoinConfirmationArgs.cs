da119f65741af4d15dc5647cb468c644a8b3298f
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:44 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Client/Events/OnFailureToReceiveJoinConfirmationArgs.cs
@@ -0,0 +1,10 @@
+﻿namespace TwitchLib.Client.Events
+{
+    using System;
+    using Exceptions;
+
+    public class OnFailureToReceiveJoinConfirmationArgs : EventArgs
+    {
+        public FailureToReceiveJoinConfirmationException Exception;
+    }
+}
