0125e71f6b31f991f7c0cc8c72f098111a416941
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:32 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api/ThirdParty/AuthorizationFlow/CreatedFlow.cs
@@ -0,0 +1,13 @@
+﻿namespace TwitchLib.Api.ThirdParty.AuthorizationFlow
+{
+    using System;
+    using Newtonsoft.Json;
+
+    public class CreatedFlow
+    {
+        [JsonProperty(PropertyName = "message")]
+        public String Url { get; protected set; }
+        [JsonProperty(PropertyName = "id")]
+        public String Id { get; protected set; }
+    }
+}
