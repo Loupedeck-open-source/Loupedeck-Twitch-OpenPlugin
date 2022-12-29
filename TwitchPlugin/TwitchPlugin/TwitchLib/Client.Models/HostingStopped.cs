ab18710fcd7bc968d3323a33166687c4b81fed1f
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:39 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Client.Models/HostingStopped.cs
@@ -0,0 +1,26 @@
+﻿namespace TwitchLib.Client.Models
+{
+    using System;
+    using Internal;
+
+    public class HostingStopped
+    {
+        /// <summary>Property representing hosting channel.</summary>
+        public String HostingChannel;
+        /// <summary>Property representing number of viewers that were in hosting channel.</summary>
+        public Int32 Viewers;
+
+        public HostingStopped(IrcMessage ircMessage)
+        {
+            var splitted = ircMessage.Message.Split(' ');
+            this.HostingChannel = ircMessage.Channel;
+            this.Viewers = splitted[1].StartsWith("-") ? 0 : Int32.Parse(splitted[1]);
+        }
+
+        public HostingStopped(String hostingChannel, Int32 viewers)
+        {
+            this.HostingChannel = hostingChannel;
+            this.Viewers = viewers;
+        }
+    }
+}
