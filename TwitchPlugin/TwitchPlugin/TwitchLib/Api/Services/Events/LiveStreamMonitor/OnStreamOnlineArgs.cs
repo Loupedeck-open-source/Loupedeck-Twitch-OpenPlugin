55f6dd5a9c4143ea0a84d861d10f4a2b3ca24c2d
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:31 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api/Services/Events/LiveStreamMonitor/OnStreamOnlineArgs.cs
@@ -0,0 +1,21 @@
+﻿namespace TwitchLib.Api.Services.Events.LiveStreamMonitor
+{
+    using System;
+    using Helix.Models.Streams;
+
+    /// <inheritdoc />
+    /// <summary>
+    /// Class representing EventArgs for OnStreamOnline event.
+    /// </summary>
+    public class OnStreamOnlineArgs : EventArgs
+    {
+        /// <summary>
+        /// Event property representing channel that has gone online.
+        /// </summary>
+        public String Channel;
+        /// <summary>
+        /// Event property representing live stream information.
+        /// </summary>
+        public Stream Stream;
+    }
+}
