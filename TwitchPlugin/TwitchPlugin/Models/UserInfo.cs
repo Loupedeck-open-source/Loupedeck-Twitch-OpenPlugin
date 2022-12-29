1892309c23ca49605ce2fba319438e8461c92cc2
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:50:54 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/Models/UserInfo.cs
@@ -0,0 +1,16 @@
+﻿namespace Loupedeck.TwitchPlugin
+{
+    using System;
+
+    public class UserInfo
+    {
+        public UserInfo(Int32 id, String login)
+        {
+            this.Id = id;
+            this.Login = login;
+        }
+
+        public Int32 Id { get; set; }
+        public String Login { get; set; }
+    }
+}
