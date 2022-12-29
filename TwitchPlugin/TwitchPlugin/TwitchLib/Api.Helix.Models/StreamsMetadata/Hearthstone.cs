1f9bd48b9e7bf00f81100efe080aa057a08e66fe
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:22 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api.Helix.Models/StreamsMetadata/Hearthstone.cs
@@ -0,0 +1,12 @@
+﻿namespace TwitchLib.Api.Helix.Models.StreamsMetadata
+{
+    using Newtonsoft.Json;
+
+    public class Hearthstone
+    {
+        [JsonProperty(PropertyName = "broadcaster")]
+        public PlayerHearthstone Broadcaster { get; protected set; }
+        [JsonProperty(PropertyName = "opponent")]
+        public PlayerHearthstone Opponent { get; protected set; }
+    }
+}
