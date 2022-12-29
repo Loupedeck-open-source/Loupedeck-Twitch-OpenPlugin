264941ac6269f71dc6807d22fdde6c394db9b466
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:05 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api.Core.Models/Undocumented/ChannelExtensionData/IconUrls.cs
@@ -0,0 +1,11 @@
+﻿namespace TwitchLib.Api.Core.Models.Undocumented.ChannelExtensionData
+{
+    using System;
+    using Newtonsoft.Json;
+
+    public class IconUrls
+    {
+        [JsonProperty(PropertyName = "100x100")]
+        public String Url100x100 { get; protected set; }
+    }
+}
