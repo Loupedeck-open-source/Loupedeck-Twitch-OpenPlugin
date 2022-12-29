58e105a7018fcf4db1fc4a6836aba42f05b7a08b
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:50:45 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/Contracts/ILogger.cs
@@ -0,0 +1,11 @@
+﻿namespace Loupedeck.TwitchPlugin
+{
+    using System;
+
+    public interface ILogger
+    {
+        void LogInformation(String message, params Object[] args);
+
+        void LogError(String message, params Object[] args);
+    }
+}
