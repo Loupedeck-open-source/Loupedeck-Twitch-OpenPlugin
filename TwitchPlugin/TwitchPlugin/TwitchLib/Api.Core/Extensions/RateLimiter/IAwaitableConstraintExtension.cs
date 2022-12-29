673f5266b966f7bef10161631852c23a58dd2bb7
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:16 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api.Core/Extensions/RateLimiter/IAwaitableConstraintExtension.cs
@@ -0,0 +1,13 @@
+﻿namespace TwitchLib.Api.Core.Extensions.RateLimiter
+{
+    using Core.RateLimiter;
+    using Interfaces;
+
+    public static class IAwaitableConstraintExtension
+    {
+        public static IAwaitableConstraint Compose(this IAwaitableConstraint ac1, IAwaitableConstraint ac2)
+        {
+            return ac1 == ac2 ? ac1 : new ComposedAwaitableConstraint(ac1, ac2);
+        }
+    }
+}
