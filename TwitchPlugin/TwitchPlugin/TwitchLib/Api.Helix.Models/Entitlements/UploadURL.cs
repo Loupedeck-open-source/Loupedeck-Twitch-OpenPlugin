10ac6c642ac7ff10f92460b72de30cf179c18b91
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:21 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api.Helix.Models/Entitlements/UploadURL.cs
@@ -0,0 +1,11 @@
+﻿namespace TwitchLib.Api.Helix.Models.Entitlements
+{
+    using System;
+    using Newtonsoft.Json;
+
+    public class UploadUrl
+    {
+        [JsonProperty(PropertyName = "url")]
+        public String Url { get; protected set; }
+    }
+}
