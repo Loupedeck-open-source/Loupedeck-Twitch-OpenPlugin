31bebdec4cd209cf03ebabe22801afe937dba360
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:26 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api.Helix.Models/Videos/GetVideosResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace TwitchLib.Api.Helix.Models.Videos
+{
+    using Common;
+    using Newtonsoft.Json;
+
+    public class GetVideosResponse
+    {
+        [JsonProperty(PropertyName = "data")]
+        public Video[] Videos { get; protected set; }
+        [JsonProperty(PropertyName = "pagination")]
+        public Pagination Pagination { get; protected set; }
+    }
+}
