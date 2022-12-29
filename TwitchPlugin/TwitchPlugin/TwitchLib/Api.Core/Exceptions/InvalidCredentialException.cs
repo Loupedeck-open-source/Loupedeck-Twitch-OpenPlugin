4b062636166d0b2fd828d8c47297a1f9223ac138
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:13 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api.Core/Exceptions/InvalidCredentialException.cs
@@ -0,0 +1,16 @@
+﻿namespace TwitchLib.Api.Core.Exceptions
+{
+    using System;
+
+    /// <inheritdoc />
+    /// <summary>Exception representing a detection that sent credentials were invalid.</summary>
+    public class InvalidCredentialException : Exception
+    {
+        /// <inheritdoc />
+        /// <summary>Exception constructor</summary>
+        public InvalidCredentialException(String data)
+            : base(data)
+        {
+        }
+    }
+}
