7276b8dac228342672856cdbc014930e76932866
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:21 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api.Helix.Models/Games/Game.cs
@@ -0,0 +1,15 @@
+﻿namespace TwitchLib.Api.Helix.Models.Games
+{
+    using System;
+    using Newtonsoft.Json;
+
+    public class Game
+    {
+        [JsonProperty(PropertyName = "id")]
+        public String Id { get; protected set; }
+        [JsonProperty(PropertyName = "name")]
+        public String Name { get; protected set; }
+        [JsonProperty(PropertyName = "box_art_url")]
+        public String BoxArtUrl { get; protected set; }
+    }
+}
