e845c31ab6913ca9285b9a6c990e6cc8af5d6b1d
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:15 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api.Core/Exceptions/UploadVideo/InvalidVideoIdException.cs
@@ -0,0 +1,16 @@
+﻿namespace TwitchLib.Api.Core.Exceptions.UploadVideo
+{
+    using System;
+
+    /// <inheritdoc />
+    /// <summary>Exception thrown when the video Id provided is invalid.</summary>
+    public class InvalidVideoIdException : Exception
+    {
+        /// <inheritdoc />
+        /// <summary>Exception constructor</summary>
+        public InvalidVideoIdException(String apiData)
+            : base(apiData)
+        {
+        }
+    }
+}
