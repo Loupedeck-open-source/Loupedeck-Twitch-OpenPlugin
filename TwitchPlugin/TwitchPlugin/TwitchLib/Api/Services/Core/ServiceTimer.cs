29c4c1b53e5ce9e845cfa127aaa785e664ef9a50
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:30 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api/Services/Core/ServiceTimer.cs
@@ -0,0 +1,28 @@
+﻿namespace TwitchLib.Api.Services.Core
+{
+    using System;
+    using System.Threading.Tasks;
+    using System.Timers;
+
+    internal class ServiceTimer : Timer
+    {
+        public Int32 IntervalInSeconds { get; }
+
+        public delegate Task ServiceTimerTick();
+
+        private readonly ServiceTimerTick _serviceTimerTickAsyncCallback;
+
+        public ServiceTimer(ServiceTimerTick serviceTimerTickAsyncCallback, Int32 intervalInSeconds = 60)
+        {
+            this._serviceTimerTickAsyncCallback = serviceTimerTickAsyncCallback;
+            this.Interval = intervalInSeconds * 1000;
+            this.IntervalInSeconds = intervalInSeconds;
+            this.Elapsed += this.TimerElapsedAsync;
+        }
+
+        private async void TimerElapsedAsync(Object sender, ElapsedEventArgs e)
+        {
+            await this._serviceTimerTickAsyncCallback();
+        }
+    }
+}
