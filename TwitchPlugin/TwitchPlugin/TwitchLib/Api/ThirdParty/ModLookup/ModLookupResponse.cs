123b04fb1c58a72034de37805c085006212c130c
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:33 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api/ThirdParty/ModLookup/ModLookupResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace TwitchLib.Api.ThirdParty.ModLookup
+{
+    using System;
+    using Newtonsoft.Json;
+
+    public class ModLookupResponse
+    {
+        [JsonProperty(PropertyName = "status")]
+        public Int32 Status { get; protected set; }
+        [JsonProperty(PropertyName = "user")]
+        public String User { get; protected set; }
+        [JsonProperty(PropertyName = "count")]
+        public Int32 Count { get; protected set; }
+        [JsonProperty(PropertyName = "channels")]
+        public ModLookupListing[] Channels { get; protected set; }
+    }
+}
