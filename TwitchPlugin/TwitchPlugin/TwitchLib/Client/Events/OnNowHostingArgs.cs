666102dda22727d3995b5ab74cc753787ca871aa
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:47 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Client/Events/OnNowHostingArgs.cs
@@ -0,0 +1,14 @@
+﻿namespace TwitchLib.Client.Events
+{
+    using System;
+
+    /// <inheritdoc />
+    /// <summary>Args representing the detected hosted channel.</summary>
+    public class OnNowHostingArgs : EventArgs
+    {
+        /// <summary>Property the channel that received the event.</summary>
+        public String Channel;
+        /// <summary>Property representing channel that is being hosted.</summary>
+        public String HostedChannel;
+    }
+}
