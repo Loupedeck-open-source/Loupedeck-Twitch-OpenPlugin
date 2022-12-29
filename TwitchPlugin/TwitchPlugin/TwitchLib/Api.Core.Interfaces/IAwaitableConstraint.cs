5fcf5bed6c63135fa7a70e3057e79c369ecdb372
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:01 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api.Core.Interfaces/IAwaitableConstraint.cs
@@ -0,0 +1,11 @@
+﻿namespace TwitchLib.Api.Core.Interfaces
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public interface IAwaitableConstraint
+    {
+        Task<IDisposable> WaitForReadiness(CancellationToken cancellationToken);
+    }
+}
