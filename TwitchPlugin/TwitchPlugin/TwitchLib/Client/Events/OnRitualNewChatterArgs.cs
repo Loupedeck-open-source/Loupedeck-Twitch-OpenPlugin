c575523fbd9c0c82e5c0f9d4501704aca465ddbc
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:47 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Client/Events/OnRitualNewChatterArgs.cs
@@ -0,0 +1,10 @@
+﻿namespace TwitchLib.Client.Events
+{
+    using System;
+    using Models;
+
+    public class OnRitualNewChatterArgs : EventArgs
+    {
+        public RitualNewChatter RitualNewChatter;
+    }
+}
