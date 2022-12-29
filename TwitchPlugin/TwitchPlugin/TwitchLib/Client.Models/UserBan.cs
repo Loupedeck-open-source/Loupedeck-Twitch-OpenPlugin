f773f090eae1c71856d7d8fba69b01b64f797775
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:42 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Client.Models/UserBan.cs
@@ -0,0 +1,35 @@
+﻿namespace TwitchLib.Client.Models
+{
+    using System;
+    using Internal;
+
+    public class UserBan
+    {
+        /// <summary>Reason for ban, if it was provided.</summary>
+        public String BanReason;
+        /// <summary>Channel that had ban event.</summary>
+        public String Channel;
+        /// <summary>User that was banned.</summary>
+        public String Username;
+
+
+        public UserBan(IrcMessage ircMessage)
+        {
+            this.Channel = ircMessage.Channel;
+            this.Username = ircMessage.Message;
+
+            var successBanReason = ircMessage.Tags.TryGetValue(Tags.BanReason, out var banReason);
+            if (successBanReason)
+            {
+                this.BanReason = banReason;
+            }
+        }
+
+        public UserBan(String channel, String username, String banReason)
+        {
+            this.Channel = channel;
+            this.Username = username;
+            this.BanReason = banReason;
+        }
+    }
+}
