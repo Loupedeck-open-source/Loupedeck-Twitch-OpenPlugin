b0a82b245ac4a33f6b343f6e3798925ddde970d1
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:01 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api.Core.Interfaces/IFollows.cs
@@ -0,0 +1,11 @@
+﻿namespace TwitchLib.Api.Core.Interfaces
+{
+    using System;
+
+    public interface IFollows
+    {
+        Int32 Total { get; }
+        String Cursor { get; }
+        IFollow[] Follows { get; }
+    }
+}
