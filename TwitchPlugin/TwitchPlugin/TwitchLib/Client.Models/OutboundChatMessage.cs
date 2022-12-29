388566674675568f94a1753a1c2c6f092ea4b213
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:40 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Client.Models/OutboundChatMessage.cs
@@ -0,0 +1,18 @@
+﻿namespace TwitchLib.Client.Models
+{
+    using System;
+
+    public class OutboundChatMessage
+    {        
+        public String Channel { get; set; }
+        public String Message { get; set; }
+        public String Username { get; set; }
+
+        public override String ToString()
+        {
+            var user = this.Username.ToLower();
+            var channel = this.Channel.ToLower();
+            return $":{user}!{user}@{user}.tmi.twitch.tv PRIVMSG #{channel} :{this.Message}";
+        }
+    }
+}
