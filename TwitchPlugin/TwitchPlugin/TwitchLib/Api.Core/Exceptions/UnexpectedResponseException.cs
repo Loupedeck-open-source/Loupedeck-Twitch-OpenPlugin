c6474b2c2638708b47b78f8722aa5ac632b852e0
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:14 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api.Core/Exceptions/UnexpectedResponseException.cs
@@ -0,0 +1,16 @@
+﻿namespace TwitchLib.Api.Core.Exceptions
+{
+    using System;
+
+    /// <inheritdoc />
+    /// <summary>Exception representing a response received from Twitch that is not expected by the library</summary>
+    public class UnexpectedResponseException : Exception
+    {
+        /// <inheritdoc />
+        /// <summary>Exception constructor</summary>
+        public UnexpectedResponseException(String data)
+            : base(data)
+        {
+        }
+    }
+}
