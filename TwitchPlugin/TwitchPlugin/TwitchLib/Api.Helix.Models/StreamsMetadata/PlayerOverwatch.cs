afe203018eea4cc0b16c0bdeea38b1e766204aa7
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:23 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api.Helix.Models/StreamsMetadata/PlayerOverwatch.cs
@@ -0,0 +1,10 @@
+﻿namespace TwitchLib.Api.Helix.Models.StreamsMetadata
+{
+    using Newtonsoft.Json;
+
+    public class PlayerOverwatch
+    {
+        [JsonProperty(PropertyName = "hero")]
+        public HeroOverwatch Hero { get; protected set; }
+    }
+}
