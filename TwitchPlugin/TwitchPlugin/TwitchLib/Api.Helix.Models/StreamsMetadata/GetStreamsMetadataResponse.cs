c60ddfe27ae7c1a7b4efabada71dcd0601db50a8
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:22 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api.Helix.Models/StreamsMetadata/GetStreamsMetadataResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace TwitchLib.Api.Helix.Models.StreamsMetadata
+{
+    using Common;
+    using Newtonsoft.Json;
+
+    public class GetStreamsMetadataResponse
+    {
+        [JsonProperty(PropertyName = "data")]
+        public StreamMetadata[] StreamsMetadatas { get; protected set; }
+        [JsonProperty(PropertyName = "pagination")]
+        public Pagination Pagination { get; protected set; }
+    }
+}
