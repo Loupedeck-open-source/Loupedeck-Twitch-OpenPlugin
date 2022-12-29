31c06519c36cf2c6dd717c9af91d45ef17e79f85
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:05 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api.Core.Models/Undocumented/ChannelExtensionData/ExtToken.cs
@@ -0,0 +1,13 @@
+﻿namespace TwitchLib.Api.Core.Models.Undocumented.ChannelExtensionData
+{
+    using System;
+    using Newtonsoft.Json;
+
+    public class ExtToken
+    {
+        [JsonProperty(PropertyName = "extension_id")]
+        public String ExtensionId { get; protected set; }
+        [JsonProperty(PropertyName = "token")]
+        public String Token { get; protected set; }
+    }
+}
