43dd66c79cdad70a02a106f75483d6e53d65206f
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:46 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Client/Events/OnMessageReceivedArgs.cs
@@ -0,0 +1,13 @@
+﻿namespace TwitchLib.Client.Events
+{
+    using System;
+    using Models;
+
+    /// <inheritdoc />
+    /// <summary>Args representing message received event.</summary>
+    public class OnMessageReceivedArgs : EventArgs
+    {
+        /// <summary>Property representing received chat message.</summary>
+        public ChatMessage ChatMessage;
+    }
+}
