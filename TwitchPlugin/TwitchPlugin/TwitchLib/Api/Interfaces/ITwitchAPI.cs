1b8e6fa080bdb4795ceeaba7f9dbb12816fe4011
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:29 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api/Interfaces/ITwitchAPI.cs
@@ -0,0 +1,13 @@
+﻿namespace TwitchLib.Api.Interfaces
+{
+    using Core.Interfaces;
+    using Core.Undocumented;
+
+    public interface ITwitchAPI
+    {
+        IApiSettings Settings { get; }
+        Helix.Helix Helix { get; }
+        ThirdParty.ThirdParty ThirdParty { get; }
+        Undocumented Undocumented { get; }
+    }
+}
