7d7dca95c0190da3219415096b11cd2303035148
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:50:53 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/Models/CreateMarkerResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Loupedeck.TwitchPlugin
+{
+    using Newtonsoft.Json;
+
+    public class CreateMarkerResponse
+    {
+        [JsonProperty(PropertyName = "data")] 
+        public Marker[] Markers { get; protected set; }
+    }
+}
