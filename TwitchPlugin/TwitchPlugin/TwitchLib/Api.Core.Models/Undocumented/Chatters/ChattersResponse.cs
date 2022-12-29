340f2002fb761dbffd4b417dd9981287b9097e00
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:07 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api.Core.Models/Undocumented/Chatters/ChattersResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace TwitchLib.Api.Core.Models.Undocumented.Chatters
+{
+    using System;
+    using Newtonsoft.Json;
+
+    public class ChattersResponse
+    {
+        [JsonProperty(PropertyName = "chatter_count")]
+        public Int32 ChatterCount { get; protected set; }
+        [JsonProperty(PropertyName = "chatters")]
+        public Chatters Chatters { get; protected set; }
+    }
+}
