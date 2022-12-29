45ad8db471f701c9642944036c69a22608c29aec
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:00 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api.Core.Interfaces/IApiSettings.cs
@@ -0,0 +1,19 @@
+﻿namespace TwitchLib.Api.Core.Interfaces
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using Enums;
+
+    public interface IApiSettings
+    {
+        String AccessToken { get; set; }
+        String Secret { get; set; }
+        String ClientId { get; set; }
+        Boolean SkipDynamicScopeValidation { get; set; }
+        Boolean SkipAutoServerTokenGeneration { get; set; }
+        List<AuthScopes> Scopes { get; set; }
+
+        event PropertyChangedEventHandler PropertyChanged;
+    }
+}
