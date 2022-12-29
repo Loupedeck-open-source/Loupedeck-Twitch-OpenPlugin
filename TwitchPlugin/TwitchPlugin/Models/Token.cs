e1c6d68299c541f8283e9dc9d91dc0f6654723fa
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:50:53 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/Models/Token.cs
@@ -0,0 +1,18 @@
+﻿namespace Loupedeck.TwitchPlugin
+{
+    using System;
+    using System.Collections.Generic;
+    using Newtonsoft.Json;
+
+    public class Token
+    {
+        [JsonProperty("access_token")]
+        public String AccessToken { get; set; }
+
+        [JsonProperty("refresh_token")]
+        public String RefreshToken { get; set; }
+
+        [JsonProperty("scope")]
+        public List<String> Scopes { get; set; }
+    }
+}
