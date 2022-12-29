c6b085282204e1bffdc28970ff4bf5c097377cfe
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:39 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Client.Models/Internal/Tags.cs
@@ -0,0 +1,50 @@
+﻿namespace TwitchLib.Client.Models.Internal
+{
+    using System;
+
+    public static class Tags
+    {
+        public const String Badges = "badges";
+        public const String BanDuration = "ban-duration";
+        public const String BanReason = "ban-reason";
+        public const String BroadcasterLang = "broadcaster-lang";
+        public const String Bits = "bits";
+        public const String Color = "color";
+        public const String DisplayName = "display-name";
+        public const String Emotes = "emotes";
+        public const String EmoteOnly = "emote-only";
+        public const String EmotesSets = "emote-sets";
+        public const String FollowersOnly = "followers-only";
+        public const String Id = "id";
+        public const String Login = "login";
+        public const String Mercury = "mercury";
+        public const String MessageId = "message-id";
+        public const String Mod = "mod";
+        public const String MsgId = "msg-id";
+        public const String MsgParamDisplayname = "msg-param-displayName";
+        public const String MsgParamLogin = "msg-param-login";
+        public const String MsgParamMonths = "msg-param-months";
+        public const String MsgParamSubPlan = "msg-param-sub-plan";
+        public const String MsgParamSubPlanName = "msg-param-sub-plan-name";
+        public const String MsgParamViewerCount = "msg-param-viewerCount";
+        public const String MsgParamRecipientDisplayname = "msg-param-recipient-display-name";
+        public const String MsgParamRecipientId = "msg-param-recipient-id";
+        public const String MsgParamRecipientUsername = "msg-param-recipient-user-name";
+        public const String MsgParamRitualName = "msg-param-ritual-name";
+        public const String MsgParamMassGiftCount = "msg-param-mass-gift-count";
+        public const String MsgParamSenderCount = "msg-param-sender-count";
+        public const String Noisy = "noisy";
+        public const String Rituals = "rituals";
+        public const String RoomId = "room-id";
+        public const String R9K = "r9k";
+        public const String Slow = "slow";
+        public const String Subscriber = "subscriber";
+        public const String SubsOnly = "subs-only";
+        public const String SystemMsg = "system-msg";
+        public const String ThreadId = "thread-id";
+        public const String TmiSentTs = "tmi-sent-ts";
+        public const String Turbo = "turbo";
+        public const String UserId = "user-id";
+        public const String UserType = "user-type";
+    }
+}
