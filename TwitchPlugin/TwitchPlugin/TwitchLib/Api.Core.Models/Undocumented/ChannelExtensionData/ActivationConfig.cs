f93a68d41254f1845818e68d65aad1356aac845c
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:04 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api.Core.Models/Undocumented/ChannelExtensionData/ActivationConfig.cs
@@ -0,0 +1,13 @@
+﻿namespace TwitchLib.Api.Core.Models.Undocumented.ChannelExtensionData
+{
+    using System;
+    using Newtonsoft.Json;
+
+    public class ActivationConfig
+    {
+        [JsonProperty(PropertyName = "slot")]
+        public String Slot { get; protected set; }
+        [JsonProperty(PropertyName = "anchor")]
+        public String Anchor { get; protected set; }
+    }
+}
