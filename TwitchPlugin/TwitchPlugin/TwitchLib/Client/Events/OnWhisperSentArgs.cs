096471ec298428aee882843e12a7cc360199fbc0
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:49 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Client/Events/OnWhisperSentArgs.cs
@@ -0,0 +1,16 @@
+﻿namespace TwitchLib.Client.Events
+{
+    using System;
+
+    /// <inheritdoc />
+    /// <summary>Args representing whisper sent event.</summary>
+    public class OnWhisperSentArgs : EventArgs
+    {
+        /// <summary>Property representing username of bot.</summary>
+        public String Username;
+        /// <summary>Property representing receiver of the whisper.</summary>
+        public String Receiver;
+        /// <summary>Property representing sent message contents.</summary>
+        public String Message;
+    }
+}
