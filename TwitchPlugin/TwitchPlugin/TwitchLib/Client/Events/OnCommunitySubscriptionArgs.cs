2d3468fe78db089c667d7d896ab73f8f37293868
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:43 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Client/Events/OnCommunitySubscriptionArgs.cs
@@ -0,0 +1,13 @@
+﻿namespace TwitchLib.Client.Events
+{
+    using System;
+    using Models;
+
+    public class OnCommunitySubscriptionArgs
+    {
+        /// <summary>Property representing the information of the community subscription.</summary>
+        public CommunitySubscription GiftedSubscription;
+        /// <summary>Property representing the Twitch channel this event fired from.</summary>
+        public String Channel;
+    }
+}
