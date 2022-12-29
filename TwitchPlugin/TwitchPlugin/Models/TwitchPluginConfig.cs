1d2fc285c8ec98b20daabaaaf2ced1aa27a64620
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:50:54 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/Models/TwitchPluginConfig.cs
@@ -0,0 +1,13 @@
+﻿namespace Loupedeck.TwitchPlugin
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class TwitchPluginConfig
+    {
+        public String ClientId { get; set; }
+        public String ClientSecret { get; set; }
+        public Int32 Timeout { get; set; }
+        public List<Int32> Ports { get; set; }
+    }
+}
