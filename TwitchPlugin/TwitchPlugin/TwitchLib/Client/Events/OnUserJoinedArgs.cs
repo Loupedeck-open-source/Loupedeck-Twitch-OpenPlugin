422e2d7fd09e1a24b9fae8117e5b7e5ee911e5fd
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:48 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Client/Events/OnUserJoinedArgs.cs
@@ -0,0 +1,14 @@
+﻿namespace TwitchLib.Client.Events
+{
+    using System;
+
+    /// <inheritdoc />
+    /// <summary>Args representing viewer joined event.</summary>
+    public class OnUserJoinedArgs : EventArgs
+    {
+        /// <summary>Property representing username of joined viewer.</summary>
+        public String Username;
+        /// <summary>Property representing channel bot is connected to.</summary>
+        public String Channel;
+    }
+}
