6ce8b644781b34cce4fff122c03eda73c50793b4
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:20 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api.Helix.Models/Clips/CreateClip/CreatedClip.cs
@@ -0,0 +1,13 @@
+﻿namespace TwitchLib.Api.Helix.Models.Clips.CreateClip
+{
+    using System;
+    using Newtonsoft.Json;
+
+    public class CreatedClip
+    {
+        [JsonProperty(PropertyName = "id")]
+        public String Id { get; protected set; }
+        [JsonProperty(PropertyName = "edit_url")]
+        public String EditUrl { get; protected set; }
+    }
+}
