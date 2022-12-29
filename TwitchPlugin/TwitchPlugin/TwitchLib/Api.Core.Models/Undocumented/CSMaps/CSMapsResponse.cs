ef46ee8f84b5f0b56bb553e2ff5f3eb7a3592958
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:03 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api.Core.Models/Undocumented/CSMaps/CSMapsResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace TwitchLib.Api.Core.Models.Undocumented.CSMaps
+{
+    using System;
+    using Newtonsoft.Json;
+
+    public class CSMapsResponse
+    {
+        [JsonProperty(PropertyName = "_total")]
+        public Int32 Total { get; protected set; }
+        [JsonProperty(PropertyName = "maps")]
+        public Map[] Maps { get; protected set; }
+    }
+}
