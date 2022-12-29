bc6aba8ac098cd2204d440ab8eb801854e85dd3a
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:12 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api.Core/Exceptions/BadGatewayException.cs
@@ -0,0 +1,16 @@
+﻿namespace TwitchLib.Api.Core.Exceptions
+{
+    using System;
+
+    /// <inheritdoc />
+    /// <summary>Exception representing a 502 Http Statuscode</summary>
+    public class BadGatewayException : Exception
+    {
+        /// <inheritdoc />
+        /// <summary>Exception constructor</summary>
+        public BadGatewayException(String data)
+            : base(data)
+        {
+        }
+    }
+}
