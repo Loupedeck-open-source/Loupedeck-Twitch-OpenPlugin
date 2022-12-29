ad8dd45290069e72ecc47d9f16fd212905cffe9a
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:45 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Client/Events/OnLeftChannelArgs.cs
@@ -0,0 +1,14 @@
+﻿namespace TwitchLib.Client.Events
+{
+    using System;
+
+    /// <inheritdoc />
+    /// <summary>Args representing the client left a channel event.</summary>
+    public class OnLeftChannelArgs : EventArgs
+    {
+        /// <summary>The username of the bot that left the channel.</summary>
+        public String BotUsername;
+        /// <summary>Channel that bot just left (parted).</summary>
+        public String Channel;
+    }
+}
