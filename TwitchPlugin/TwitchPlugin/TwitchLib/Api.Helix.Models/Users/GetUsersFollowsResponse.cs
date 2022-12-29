1a75c9cccef09d2f8a254c66fedaa16ad8215297
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:24 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api.Helix.Models/Users/GetUsersFollowsResponse.cs
@@ -0,0 +1,16 @@
+﻿namespace TwitchLib.Api.Helix.Models.Users
+{
+    using System;
+    using Common;
+    using Newtonsoft.Json;
+
+    public class GetUsersFollowsResponse
+    {
+        [JsonProperty(PropertyName = "data")]
+        public Follow[] Follows { get; protected set; }
+        [JsonProperty(PropertyName = "pagination")]
+        public Pagination Pagination { get; protected set; }
+        [JsonProperty(PropertyName = "total")]
+        public Int64 TotalFollows { get; protected set; }
+    }
+}
