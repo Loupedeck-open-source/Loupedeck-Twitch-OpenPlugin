e209afcbd5f0bdd15abeeb95e4432a878c64a9fe
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:51 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Client/Exceptions/InvalidParameterException.cs
@@ -0,0 +1,20 @@
+﻿namespace TwitchLib.Client.Exceptions
+{
+    using System;
+
+    /// <inheritdoc />
+    /// <summary>Exception representing bad information being provided to function/method.</summary>
+    public class InvalidParameterException : Exception
+    {
+        /// <summary>Username that had the exception.</summary>
+        public String Username { get; protected set; }
+
+        /// <inheritdoc />
+        /// <summary>Exception construtor.</summary>
+        public InvalidParameterException(String reasoning, String twitchUsername)
+            : base(reasoning)
+        {
+            this.Username = twitchUsername;
+        }
+    }
+}
