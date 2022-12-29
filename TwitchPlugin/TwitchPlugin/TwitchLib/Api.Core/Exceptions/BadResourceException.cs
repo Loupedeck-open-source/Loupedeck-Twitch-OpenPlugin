b8f0d9fe4a849b92b0346f723addb053a23a4987
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:12 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api.Core/Exceptions/BadResourceException.cs
@@ -0,0 +1,16 @@
+﻿namespace TwitchLib.Api.Core.Exceptions
+{
+    using System;
+
+    /// <inheritdoc />
+    /// <summary>Exception representing an invalid resource</summary>
+    public class BadResourceException : Exception
+    {
+        /// <inheritdoc />
+        /// <summary>Exception constructor</summary>
+        public BadResourceException(String apiData)
+            : base(apiData)
+        {
+        }
+    }
+}
