46bf2a4d56def862ca169e012ee25bd07a37cb54
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:50 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Client/Exceptions/IllegalAssignmentException.cs
@@ -0,0 +1,16 @@
+﻿namespace TwitchLib.Client.Exceptions
+{
+    using System;
+
+    /// <inheritdoc />
+    /// <summary>Exception thrown when attempting to assign a variable with a different value that is not allowed.</summary>
+    public class IllegalAssignmentException : Exception
+    {
+        /// <inheritdoc />
+        /// <summary>Exception constructor</summary>
+        public IllegalAssignmentException(String description)
+            : base(description)
+        {
+        }
+    }
+}
