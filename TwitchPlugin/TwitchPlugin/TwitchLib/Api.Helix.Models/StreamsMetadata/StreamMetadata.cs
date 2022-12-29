4ffe8f79ec2312d3dcc388ebf831503918bd244b
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:23 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api.Helix.Models/StreamsMetadata/StreamMetadata.cs
@@ -0,0 +1,17 @@
+﻿namespace TwitchLib.Api.Helix.Models.StreamsMetadata
+{
+    using System;
+    using Newtonsoft.Json;
+
+    public class StreamMetadata
+    {
+        [JsonProperty(PropertyName = "user_id")]
+        public String UserId { get; protected set; }
+        [JsonProperty(PropertyName = "game_id")]
+        public String GameId { get; protected set; }
+        [JsonProperty(PropertyName = "hearthstone")]
+        public Hearthstone Hearthstone { get; protected set; }
+        [JsonProperty(PropertyName = "overwatch")]
+        public Overwatch Overwatch { get; protected set; }
+    }
+}
