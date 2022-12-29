fd35ed082ac1e0fab3ad35bf496520ef7c63181c
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:13 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api.Core/Exceptions/InternalServerErrorException.cs
@@ -0,0 +1,16 @@
+﻿namespace TwitchLib.Api.Core.Exceptions
+{
+    using System;
+
+    /// <inheritdoc />
+    /// <summary>Exception representing a 500 Http Statuscode</summary>
+    public class InternalServerErrorException : Exception
+    {
+        /// <inheritdoc />
+        /// <summary>Exception constructor</summary>
+        public InternalServerErrorException(String data)
+            : base(data)
+        {
+        }
+    }
+}
