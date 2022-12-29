f7a2bc9c89aec2a9978a2e5695bf8862c3487ae9
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:57 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Communication/Events/OnSendFailedEventArgs.cs
@@ -0,0 +1,10 @@
+﻿namespace TwitchLib.Communication.Events
+{
+    using System;
+
+    public class OnSendFailedEventArgs : EventArgs
+    {
+        public String Data;
+        public Exception Exception;
+    }
+}
