bb9ee82926290bb080941d51855d65331be65f25
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:44 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Client/Events/OnConnectedArgs.cs
@@ -0,0 +1,14 @@
+﻿namespace TwitchLib.Client.Events
+{
+    using System;
+
+    /// <inheritdoc />
+    /// <summary>Args representing on connected event.</summary>
+    public class OnConnectedArgs : EventArgs
+    {
+        /// <summary>Property representing bot username.</summary>
+        public String BotUsername;
+        /// <summary>Property representing connected channel.</summary>
+        public String AutoJoinChannel;
+    }
+}
