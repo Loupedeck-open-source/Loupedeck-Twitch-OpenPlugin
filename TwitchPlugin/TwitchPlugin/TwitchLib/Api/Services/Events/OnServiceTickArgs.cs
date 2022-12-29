2b4ba5033f686200f6b3e62326d9689614024bb1
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:32 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api/Services/Events/OnServiceTickArgs.cs
@@ -0,0 +1,12 @@
+﻿namespace TwitchLib.Api.Services.Events
+{
+    using System;
+
+    /// <inheritdoc />
+    /// <summary>
+    /// Class representing EventArgs for the OnServiceTick event.
+    /// </summary>
+    public class OnServiceTickArgs : EventArgs
+    {
+    }
+}
