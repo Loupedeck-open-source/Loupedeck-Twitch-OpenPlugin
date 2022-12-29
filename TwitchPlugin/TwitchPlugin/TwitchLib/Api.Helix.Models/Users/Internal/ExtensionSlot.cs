885aaa84eabba4904e719ba8d94f9030aaf3ca5f
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:25 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api.Helix.Models/Users/Internal/ExtensionSlot.cs
@@ -0,0 +1,19 @@
+﻿namespace TwitchLib.Api.Helix.Models.Users.Internal
+{
+    using System;
+    using Core.Enums;
+
+    public class ExtensionSlot
+    {
+        public ExtensionType Type;
+        public String Slot;
+        public UserExtensionState UserExtensionState;
+
+        public ExtensionSlot(ExtensionType type, String slot, UserExtensionState userExtensionState)
+        {
+            this.Type = type;
+            this.Slot = slot;
+            this.UserExtensionState = userExtensionState;
+        }
+    }
+}
