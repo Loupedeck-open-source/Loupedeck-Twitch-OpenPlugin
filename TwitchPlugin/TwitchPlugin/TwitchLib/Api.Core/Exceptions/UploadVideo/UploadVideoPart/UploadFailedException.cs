df0f08005e97ca1cf90d211bb54652bffea4e29d
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:16 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api.Core/Exceptions/UploadVideo/UploadVideoPart/UploadFailedException.cs
@@ -0,0 +1,16 @@
+﻿namespace TwitchLib.Api.Core.Exceptions.UploadVideo.UploadVideoPart
+{
+    using System;
+
+    /// <inheritdoc />
+    /// <summary>Thrown when Twitch reports a failure of the upload.</summary>
+    public class UploadFailedException : Exception
+    {
+        /// <inheritdoc />
+        /// <summary>Exception constructor</summary>
+        public UploadFailedException(String apiData)
+            : base(apiData)
+        {
+        }
+    }
+}
