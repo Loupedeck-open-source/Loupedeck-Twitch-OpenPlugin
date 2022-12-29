8749080114d721b7356f83f3d0c0fcdff6554c82
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:01 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api.Core.Interfaces/IUser.cs
@@ -0,0 +1,17 @@
+﻿namespace TwitchLib.Api.Core.Interfaces
+{
+    using System;
+
+    public interface IUser
+    {
+        String Id { get; }
+        String Bio { get; }
+        DateTime CreatedAt { get; }
+        String DisplayName { get; }
+        String Logo { get; }
+        String Name { get; }
+        String Type { get; }
+        DateTime UpdatedAt { get; }
+
+    }
+}
