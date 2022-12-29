c95c8ad7159f3282689fb1d2d21302814b45764c
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:04 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api.Core.Models/Undocumented/CSStreams/LocalizedGame.cs
@@ -0,0 +1,25 @@
+﻿namespace TwitchLib.Api.Core.Models.Undocumented.CSStreams
+{
+    using System;
+    using Newtonsoft.Json;
+
+    public class LocalizedGame
+    {
+        [JsonProperty(PropertyName = "name")]
+        public String Name { get; protected set; }
+        [JsonProperty(PropertyName = "popularity")]
+        public Int32 Popularity { get; protected set; }
+        [JsonProperty(PropertyName = "_id")]
+        public String Id { get; protected set; }
+        [JsonProperty(PropertyName = "giantbomb_id")]
+        public String GiantbombId { get; protected set; }
+        [JsonProperty(PropertyName = "box")]
+        public Box Box { get; protected set; }
+        [JsonProperty(PropertyName = "logo")]
+        public Logo Logo { get; protected set; }
+        [JsonProperty(PropertyName = "localized_name")]
+        public String LocalizedName { get; protected set; }
+        [JsonProperty(PropertyName = "locale")]
+        public String Locale { get; protected set; }
+    }
+}
