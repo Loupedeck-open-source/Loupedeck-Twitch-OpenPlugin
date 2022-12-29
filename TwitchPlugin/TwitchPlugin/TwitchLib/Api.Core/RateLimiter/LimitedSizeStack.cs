1d461985c91fcc39c2eaddd0e135b05e5ae32487
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:17 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api.Core/RateLimiter/LimitedSizeStack.cs
@@ -0,0 +1,22 @@
+﻿namespace TwitchLib.Api.Core.RateLimiter
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class LimitedSizeStack<T>: LinkedList<T>
+    {
+        private readonly Int32 _maxSize;
+        public LimitedSizeStack(Int32 maxSize)
+        {
+            this._maxSize = maxSize;
+        }
+
+        public void Push(T item)
+        {
+            this.AddFirst(item);
+
+            if (this.Count > this._maxSize)
+                this.RemoveLast();
+        }
+    }
+}
