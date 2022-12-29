ee704a08717f15b31feb7230d0e803fa6de995b2
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:24 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api.Helix.Models/Users/GetUserExtensionsResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace TwitchLib.Api.Helix.Models.Users
+{
+    using Internal;
+    using Newtonsoft.Json;
+
+    public class GetUserExtensionsResponse
+    {
+        [JsonProperty(PropertyName = "data")]
+        public UserExtension[] Users { get; protected set; }
+    }
+}
