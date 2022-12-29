6db4fbca8534085b48c54479fe01fb5daa311103
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:49 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Client/Events/OnWhisperCommandReceivedArgs.cs
@@ -0,0 +1,13 @@
+﻿namespace TwitchLib.Client.Events
+{
+    using System;
+    using Models;
+
+    /// <inheritdoc />
+    /// <summary>Args representing whisper command received event.</summary>
+    public class OnWhisperCommandReceivedArgs : EventArgs
+    {
+        /// <summary>Property representing received command.</summary>
+        public WhisperCommand Command;
+    }
+}
