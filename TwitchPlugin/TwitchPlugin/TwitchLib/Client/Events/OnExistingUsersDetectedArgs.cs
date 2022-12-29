cec7e27f775d66756348ecef3b533ee8cdd1bcf7
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:44 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Client/Events/OnExistingUsersDetectedArgs.cs
@@ -0,0 +1,15 @@
+﻿namespace TwitchLib.Client.Events
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <inheritdoc />
+    /// <summary>Args representing existing user(s) detected event.</summary>
+    public class OnExistingUsersDetectedArgs : EventArgs
+    {
+        /// <summary>Property representing string list of existing users.</summary>
+        public List<String> Users;
+        /// <summary>Property representing channel bot is connected to.</summary>
+        public String Channel;
+    }
+}
