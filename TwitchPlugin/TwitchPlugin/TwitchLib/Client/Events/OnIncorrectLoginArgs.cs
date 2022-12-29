cdc84f912e603bf1a87e353c83a1f607e624f71e
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:45 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Client/Events/OnIncorrectLoginArgs.cs
@@ -0,0 +1,13 @@
+﻿namespace TwitchLib.Client.Events
+{
+    using System;
+    using Exceptions;
+
+    /// <inheritdoc />
+    /// <summary>Args representing an incorrect login event.</summary>
+    public class OnIncorrectLoginArgs : EventArgs
+    {
+        /// <summary>Property representing exception object.</summary>
+        public ErrorLoggingInException Exception;
+    }
+}
