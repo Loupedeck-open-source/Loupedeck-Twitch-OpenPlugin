51026f69fdcf4ad477063ebe475a7e80653766f5
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:07 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api.Core.Models/Undocumented/ClipChat/GetClipChatResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace TwitchLib.Api.Core.Models.Undocumented.ClipChat
+{
+    using Newtonsoft.Json;
+
+    public class GetClipChatResponse
+    {
+        [JsonProperty(PropertyName = "data")]
+        public ReChatMessage[] Messages { get; protected set; }
+    }
+}
