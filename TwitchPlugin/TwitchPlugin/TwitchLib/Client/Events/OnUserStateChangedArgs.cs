6a23e586454245d96be76b6db70a16df3f2e885e
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:48 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Client/Events/OnUserStateChangedArgs.cs
@@ -0,0 +1,13 @@
+﻿namespace TwitchLib.Client.Events
+{
+    using System;
+    using Models;
+
+    /// <inheritdoc />
+    /// <summary>Args representing on user state changed event.</summary>
+    public class OnUserStateChangedArgs : EventArgs
+    {
+        /// <summary>Property representing user state object.</summary>
+        public UserState UserState;
+    }
+}
