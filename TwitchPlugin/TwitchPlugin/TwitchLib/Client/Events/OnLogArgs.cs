30f545f95f56f193ff1eeaad6e59e44bb722fb8e
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:46 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Client/Events/OnLogArgs.cs
@@ -0,0 +1,11 @@
+﻿namespace TwitchLib.Client.Events
+{
+    using System;
+
+    public class OnLogArgs : EventArgs
+    {
+        public String BotUsername;
+        public String Data;
+        public DateTime DateTime;
+    }
+}
