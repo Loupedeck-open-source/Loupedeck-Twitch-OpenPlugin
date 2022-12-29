cb611c9e073c0331920c6ac56afefcd500ae051b
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:04 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api.Core.Models/Undocumented/CSStreams/CSStreams.cs
@@ -0,0 +1,13 @@
+﻿namespace TwitchLib.Api.Core.Models.Undocumented.CSStreams
+{
+    using System;
+    using Newtonsoft.Json;
+
+    public class CSStreams
+    {
+        [JsonProperty(PropertyName = "_total")]
+        public Int32 Total { get; protected set; }
+        [JsonProperty(PropertyName = "streams")]
+        public CSStream[] Streams { get; protected set; }
+    }
+}
