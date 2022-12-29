ff59195daedf59d6612f91b51a8bd5d6800d9e3b
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:19 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api.Helix.Models/Analytics/GetGameAnalyticsResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace TwitchLib.Api.Helix.Models.Analytics
+{
+    using Newtonsoft.Json;
+
+    public class GetGameAnalyticsResponse
+    {
+        [JsonProperty(PropertyName = "data")]
+        public GameAnalytics[] Data { get; protected set; }
+        [JsonProperty(PropertyName = "pagination")]
+        public Common.Pagination Pagination { get; protected set; }
+    }
+}
