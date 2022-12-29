de39d8455d9050c3c2bd1277029abe26883c153d
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:26 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api.Helix.Models/Webhooks/GetWebhookSubscriptionsResponse.cs
@@ -0,0 +1,16 @@
+﻿namespace TwitchLib.Api.Helix.Models.Webhooks
+{
+    using System;
+    using Common;
+    using Newtonsoft.Json;
+
+    public class GetWebhookSubscriptionsResponse
+    {
+        [JsonProperty(PropertyName = "total")]
+        public Int32 Total { get; protected set; }
+        [JsonProperty(PropertyName = "data")]
+        public Subscription[] Subscriptions { get; protected set; }
+        [JsonProperty(PropertyName = "pagination")]
+        public Pagination Pagination { get; protected set; }
+    }
+}
