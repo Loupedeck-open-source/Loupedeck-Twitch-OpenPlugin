1a06d3094825e7dbcf720dc825a31df7ae19d3d0
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:15 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api.Core/Exceptions/UploadVideo/UploadVideoPart/BadPartException.cs
@@ -0,0 +1,16 @@
+﻿namespace TwitchLib.Api.Core.Exceptions.UploadVideo.UploadVideoPart
+{
+    using System;
+
+    /// <inheritdoc />
+    /// <summary>Exception thrown when this library detects the part is invalid.</summary>
+    public class BadPartException : Exception
+    {
+        /// <inheritdoc />
+        /// <summary>Exception constructor</summary>
+        public BadPartException(String apiData)
+            : base(apiData)
+        {
+        }
+    }
+}
