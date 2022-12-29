fa7e52c434fcc27dc6d9ab7d477286305a3e1d1e
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:29 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api/Services/Core/FollowerService/CoreMonitor.cs
@@ -0,0 +1,19 @@
+﻿namespace TwitchLib.Api.Services.Core.FollowerService
+{
+    using System;
+    using System.Threading.Tasks;
+    using Helix.Models.Users;
+    using Interfaces;
+
+    internal abstract class CoreMonitor
+    {
+        protected readonly ITwitchAPI _api;
+
+        public abstract Task<GetUsersFollowsResponse> GetUsersFollowsAsync(String channel, Int32 queryCount);
+
+        protected CoreMonitor(ITwitchAPI api)
+        {
+            this._api = api;
+        }
+    }
+}
