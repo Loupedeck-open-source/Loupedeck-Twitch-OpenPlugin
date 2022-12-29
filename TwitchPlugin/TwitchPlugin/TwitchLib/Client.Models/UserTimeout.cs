28e134b9945ebe0c9c9de83461313189b06e4b2d
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:42 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Client.Models/UserTimeout.cs
@@ -0,0 +1,46 @@
+﻿namespace TwitchLib.Client.Models
+{
+    using System;
+    using Internal;
+
+    public class UserTimeout
+    {
+        /// <summary>Channel that had timeout event.</summary>
+        public String Channel;
+        /// <summary>Duration of timeout IN SECONDS.</summary>
+        public Int32 TimeoutDuration;
+        /// <summary>Reason for timeout, if it was provided.</summary>
+        public String TimeoutReason;
+        /// <summary>Viewer that was timedout.</summary>
+        public String Username;
+
+        public UserTimeout(IrcMessage ircMessage)
+        {
+            this.Channel = ircMessage.Channel;
+            this.Username = ircMessage.Message;
+
+            foreach (var tag in ircMessage.Tags.Keys)
+            {
+                var tagValue = ircMessage.Tags[tag];
+
+                switch (tag)
+                {
+                    case Tags.BanDuration:
+                        this.TimeoutDuration = Int32.Parse(tagValue);
+                        break;
+                    case Tags.BanReason:
+                        this.TimeoutReason = tagValue;
+                        break;
+                }
+            }
+        }
+
+        public UserTimeout(String channel, String username, Int32 timeoutDuration, String timeoutReason)
+        {
+            this.Channel = channel;
+            this.Username = username;
+            this.TimeoutDuration = timeoutDuration;
+            this.TimeoutReason = timeoutReason;
+        }
+    }
+}
