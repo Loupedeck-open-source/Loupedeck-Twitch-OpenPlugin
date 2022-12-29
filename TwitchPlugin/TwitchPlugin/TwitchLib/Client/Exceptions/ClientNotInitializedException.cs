9787e3adb38ec9b2ada6aeae5dd20db39707d0bf
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:50 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Client/Exceptions/ClientNotInitializedException.cs
@@ -0,0 +1,16 @@
+﻿namespace TwitchLib.Client.Exceptions
+{
+    using System;
+
+    /// <inheritdoc />
+    /// <summary>Exception thrown when attempting to assign a variable with a different value that is not allowed.</summary>
+    public class ClientNotInitializedException : Exception
+    {
+        /// <inheritdoc />
+        /// <summary>Exception constructor</summary>
+        public ClientNotInitializedException(String description)
+            : base(description)
+        {
+        }
+    }
+}
