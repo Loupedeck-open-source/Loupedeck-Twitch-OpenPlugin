820cdf0d087244eb91b9558bf653ae2abe0aa161
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:47 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Client/Events/OnNewSubscriberArgs.cs
@@ -0,0 +1,15 @@
+﻿namespace TwitchLib.Client.Events
+{
+    using System;
+    using Models;
+
+    /// <inheritdoc />
+    /// <summary>Args representing new subscriber event.</summary>
+    public class OnNewSubscriberArgs : EventArgs
+    {
+        /// <summary>Property representing subscriber object.</summary>
+        public Subscriber Subscriber;
+        /// <summary>Property representing the Twitch channel this event fired from.</summary>
+        public String Channel;
+    }
+}
