2add0114ee8416c5c7d0b428159bed268cc45286
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:06 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api.Core.Models/Undocumented/ChannelPanels/Panel.cs
@@ -0,0 +1,23 @@
+﻿namespace TwitchLib.Api.Core.Models.Undocumented.ChannelPanels
+{
+    using System;
+    using Newtonsoft.Json;
+
+    public class Panel
+    {
+        [JsonProperty(PropertyName = "_id")]
+        public String Id { get; protected set; }
+        [JsonProperty(PropertyName = "display_order")]
+        public Int32 DisplayOrder { get; protected set; }
+        [JsonProperty(PropertyName = "default")]
+        public String Kind { get; protected set; }
+        [JsonProperty(PropertyName = "html_description")]
+        public String HtmlDescription { get; protected set; }
+        [JsonProperty(PropertyName = "user_id")]
+        public String UserId { get; protected set; }
+        [JsonProperty(PropertyName = "data")]
+        public Data Data { get; protected set; }
+        [JsonProperty(PropertyName = "channel")]
+        public String Channel { get; protected set; }
+    }
+}
