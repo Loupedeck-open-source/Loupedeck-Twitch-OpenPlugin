163886524fadcf2510aefcedd480b510fd2d8fa9
Andrei Laperie <andrei.laperie@loupedeck.com>
2022-12-29 18:51:07 +0200

    initial commit

---
+++ TwitchPlugin/TwitchPlugin/TwitchLib/Api.Core.Models/Undocumented/Chatters/ChatterFormatted.cs
@@ -0,0 +1,19 @@
+﻿
+namespace TwitchLib.Api.Core.Models.Undocumented.Chatters
+{
+    using System;
+
+    public class ChatterFormatted
+    {
+        public String Username { get; protected set; }
+        public Enums.UserType UserType { get;  set; }
+
+        public ChatterFormatted(String username, Enums.UserType userType)
+        {
+            this.Username = username;
+            this.UserType = userType;
+        }
+
+        
+    }
+}
